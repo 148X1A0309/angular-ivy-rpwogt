8461e5e5a52fa35fff5634604a71572893a9c9e2
148X1A0309 <90388113+148X1A0309@users.noreply.github.com>
2022-11-26 15:34:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-rpwogt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-rpwogt)
---
+++ src/app/interpolation/interpolation.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-interpolation',
+  templateUrl: './interpolation.component.html',
+  styleUrls: ['./interpolation.component.css'],
+})
+export class InterpolationComponent implements OnInit {
+  name: string = 'special ord';
+  d: String = new Date().toLocaleTimeString();
+  //To LocalDateString returns the month/date/year
+  //To LocalTimeString will gives you the current Time locally
+  constructor() {}
+  ngOnInit() {}
+  timeid = setInterval(() => {
+    this.d = new Date().toLocaleTimeString();
+  }, 1000);
+}
