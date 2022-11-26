76b7a7b9093a72fd583f22880130336b9ba123b5
148X1A0309 <90388113+148X1A0309@users.noreply.github.com>
2022-11-26 15:52:06 +0530

    2 files modified

--- src/app/interpolation/interpolation.component.ts
+++ src/app/interpolation/interpolation.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class InterpolationComponent implements OnInit {
   name: string = 'special ord';
   d: String = new Date().toLocaleTimeString();
+  ishidden: boolean = false;
+  isdisabled: boolean = true;
   //To LocalDateString returns the month/date/year
   //To LocalTimeString will gives you the current Time locally
   constructor() {}
