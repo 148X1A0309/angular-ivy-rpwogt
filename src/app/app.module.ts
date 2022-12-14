214610dd2e70d7b46b3cf65e27305f614ba316ea
148X1A0309 <90388113+148X1A0309@users.noreply.github.com>
2022-12-14 23:20:53 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { InterpolationComponent } from './interpolation/interpolation.component';
 import { PopUpComponent } from './pop-up/pop-up.component';
+import { FormsComponent } from '../forms/forms.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ,InterpolationComponent,TasksComponent, PopUpComponent],
+  imports:      [ BrowserModule, FormsModule,ReactiveFormsModule ],
+  declarations: [ AppComponent, HelloComponent ,InterpolationComponent,TasksComponent, PopUpComponent,FormsComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
