fb0c6c82840be1519ea319229290afb572d339ed
148X1A0309 <90388113+148X1A0309@users.noreply.github.com>
2022-12-19 00:23:23 +0530

    2 files modified

--- src/app/form-array/form-array.component.ts
+++ src/app/form-array/form-array.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormArray, FormControl,Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-form-array',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormArrayComponent implements OnInit {
 
+  login=new FormGroup ({
+    Logindata:new FormArray([
+      new FormControl(null,Validators.required)
+    ])
+
+  })
   constructor() { }
 
   ngOnInit() {
+  }
+
+  Add(){
+    let mobileNums=this.login.get('Logindata') as FormArray
+
+    mobileNums.push(new FormControl(null,Validators.required))
+  }
+  
+  delete(i:number){
+    let mobileNums=this.login.get('Logindata') as FormArray
+
+    mobileNums.removeAt(i)
+    console.log(mobileNums);
   }
 
 }
